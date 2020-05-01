63f71d0c91e4607f204b0ad04a760f6ca9b0d62d
Thirupathi Peraboina <peraboinathirupathi@outlook.com>
2020-05-01 20:39:31 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,8 +2,6 @@
 
 echo 'The following "npm" command builds your Node.js/React application for'
 echo 'production in the local "build" directory (i.e. within the'
-echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
-echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 set -x
 npm run build
@@ -24,6 +22,6 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo 'Visit http://<public_id>:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
